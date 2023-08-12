c798bc7c528f55abb569705a9106297a447fe6bb
bng2kus <79231560+bng2kus@users.noreply.github.com>
2023-08-12 03:43:42 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         //listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('connected to DB & listening on port ', process.env.PORT)
+            console.log('now with CORS, connected to DB & listening on port', process.env.PORT)
         })
     })
     .catch((error) => {
